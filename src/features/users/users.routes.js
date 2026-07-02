e53c38d6ecc10c59a3499aa90f92798237ea82bd
ian <iannk918@gmail.com>
2026-07-02 23:25:31 +0900

    feat: 회원 정보 조회 API 구현

--- src/features/users/users.routes.js
+++ src/features/users/users.routes.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { requireAuth } from '../../middlewares/auth.js';
 import { validate } from '../../middlewares/validate.js';
-import { createNotImplementedController } from './users.controller.js';
+import { createNotImplementedController, getMeController } from './users.controller.js';
 import {
   changePasswordDto,
   notificationSettingsDto,
@@ -13,7 +13,7 @@ export const usersRouter = Router();
 const todo = createNotImplementedController('users');
 
 usersRouter.use(requireAuth);
-usersRouter.get('/me', todo);
+usersRouter.get('/me', getMeController);
 usersRouter.patch('/me', validate(updateMeDto), todo);
 usersRouter.delete('/me', todo);
 usersRouter.patch('/me/password', validate(changePasswordDto), todo);
--- src/features/users/users.service.js
+++ src/features/users/users.service.js
@@ -1,3 +1,16 @@
-export const placeholderService = () => {
-  return null;
+import { prisma } from '../../prisma/client.js';
+
+export const getMe = async (userId) => {
+  const user = await prisma.user.findUnique({
+    where: { id: userId },
+    select: {
+      id: true,
+      nickname: true,
+      profileImageUrl: true,
+      savingGoalText: true,
+      interestTagsJson: true,
+    },
+  });
+
+  return user;
 };
