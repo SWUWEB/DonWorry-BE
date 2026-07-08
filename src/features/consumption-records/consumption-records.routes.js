795a7383cbc6cbf5130174d8d7b98770c325558f
eunwoohwang <eunuhwang01@gmail.com>
2026-07-09 00:07:39 +0900

    style: apply prettier formatting 포맷 적용

--- src/features/consumption-records/consumption-records.routes.js
+++ src/features/consumption-records/consumption-records.routes.js
@@ -1,7 +1,10 @@
 import { Router } from 'express';
 import { requireAuth } from '../../middlewares/auth.js';
 import { validate } from '../../middlewares/validate.js';
-import { createNotImplementedController, createConsumptionRecordController } from './consumption-records.controller.js';
+import {
+  createNotImplementedController,
+  createConsumptionRecordController,
+} from './consumption-records.controller.js';
 import {
   consumptionRecordIdDto,
   createConsumptionRecordDto,
@@ -13,7 +16,11 @@ const todo = createNotImplementedController('consumption records');
 
 consumptionRecordsRouter.use(requireAuth);
 consumptionRecordsRouter.get('/', todo);
-consumptionRecordsRouter.post('/', validate(createConsumptionRecordDto), createConsumptionRecordController);
+consumptionRecordsRouter.post(
+  '/',
+  validate(createConsumptionRecordDto),
+  createConsumptionRecordController,
+);
 consumptionRecordsRouter.get('/:consumptionRecordId', validate(consumptionRecordIdDto), todo);
 consumptionRecordsRouter.patch('/:consumptionRecordId', validate(updateConsumptionRecordDto), todo);
 consumptionRecordsRouter.delete('/:consumptionRecordId', validate(consumptionRecordIdDto), todo);
