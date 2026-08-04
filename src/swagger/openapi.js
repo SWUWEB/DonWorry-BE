176806a504b22259ffa012ba98540c32afad0841
eunwoohwang <eunuhwang01@gmail.com>
2026-08-04 17:03:48 +0900

    fix: Swagger 서버 URL을 상대 경로로 수정

--- src/swagger/openapi.js
+++ src/swagger/openapi.js
@@ -58,8 +58,8 @@ export const openApiDocument = {
   },
   servers: [
     {
-      url: 'http://localhost:3000',
-      description: 'Local development server',
+      url: '/',
+      description: 'Current server',
     },
   ],
   tags: [
